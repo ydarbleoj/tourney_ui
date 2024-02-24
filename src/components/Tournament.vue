e9703fc02394b5a92952f75b783dff35d2cbdbdd
Joel Brady <brady.joel@gmail.com>
2024-02-23 18:57:36 -0800

    Update

--- src/components/Tournament.vue
+++ src/components/Tournament.vue
@@ -1,65 +1,54 @@
 <template>
-	<v-container class="pa-0 default-bg">
-		<v-row>
-			<v-col cols="3" class="text-left">
-				<BackButton
-					:routeName="'profile'"
-					:routeParams="{ id: $auth.user().id }"
-				/>
-			</v-col>
-			<v-col cols="6">
-			</v-col>
-			<v-col cols="3">
-				<div class="text-weight-regular text-right pt-2 pr-3">
-					<v-icon>
-						mdi-dots-horizontal
-					</v-icon>
-				</div>
-			</v-col>
+  <v-container class="pa-0 default-bg">
+    <v-row>
+      <v-col cols="3" class="text-left">
+        <BackButton :routeName="'profile'" :routeParams="{ id: $auth.user().id }" />
+      </v-col>
+      <v-col cols="6"> </v-col>
+      <v-col cols="3">
+        <div class="text-weight-regular text-right pt-2 pr-3">
+          <v-icon> mdi-dots-horizontal </v-icon>
+        </div>
+      </v-col>
       <div class="font-weight-regular pl-1">
-        <h1 class="text-h3 pl-5">
-          Bandon {{ year }}
-        </h1>
+        <h1 class="text-h3 pl-5">Bandon {{ year }}</h1>
       </div>
-		</v-row>
-		<v-row class="default-bg">
+    </v-row>
+    <v-row class="default-bg">
       <v-col cols="12">
-			  <leaderboards />
-        <MudderCupPreview v-if="showMudderCup"/>
-        <CourseLeaderboardPreview v-if="!loading"/>
+        <leaderboards />
+        <MudderCupPreview v-if="showMudderCup" />
+        <CourseLeaderboardPreview v-if="!loading" />
         <MoneyBreakdown />
       </v-col>
-		</v-row>
-	</v-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import { mapState } from 'pinia'
-import { mapActions } from 'pinia';
-import BackButton from './BackButton.vue'
+import { mapState } from "pinia";
+import { mapActions } from "pinia";
+import BackButton from "./BackButton.vue";
 import { useTournamentStore } from "@/store/tournament";
-import Leaderboards from './Leaderboards.vue'
-import MudderCupPreview from '../components/muddercup/LeaderboardPreview.vue'
-import CourseLeaderboardPreview from './courses/CourseLeaderboardPreview.vue';
-import MoneyBreakdown from "../components/money/TournamentBreakdown.vue"
+import Leaderboards from "./Leaderboards.vue";
+import MudderCupPreview from "../components/muddercup/LeaderboardPreview.vue";
+import CourseLeaderboardPreview from "./courses/CourseLeaderboardPreview.vue";
+import MoneyBreakdown from "../components/money/TournamentBreakdown.vue";
 
 export default {
-  name: 'Tournament',
+  name: "Tournament",
   components: {
     BackButton,
     CourseLeaderboardPreview,
-		Leaderboards,
+    Leaderboards,
     MoneyBreakdown,
-    MudderCupPreview
+    MudderCupPreview,
   },
   computed: {
-		...mapState(
-      useTournamentStore,
-      ["currentTournament", "tournaments"]
-    )
+    ...mapState(useTournamentStore, ["currentTournament", "tournaments"]),
   },
 
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: true,
@@ -69,32 +58,32 @@ export default {
       overall: false,
       showMudderCup: false,
       year: "",
-    }
+    };
   },
   methods: {
-    renderOverall (value) {
-      this.overall = value
+    renderOverall(value) {
+      this.overall = value;
     },
-    toAdmin () {
+    toAdmin() {
       this.$router.push({
-        name: 'TournamentAdmin',
+        name: "TournamentAdmin",
         params: {
-          id: this.currentTournament.id
-        }
-      })
+          id: this.currentTournament.id,
+        },
+      });
     },
     ...mapActions(useTournamentStore, ["loadTournament"]),
   },
 
-  created () {
+  created() {
     this.loadTournament(this.$route.params.id).then((response) => {
-      this.year = this.currentTournament.year
-      const cup = this.currentTournament.mudder_cup
-      this.showMudderCup = cup.mudder_cup
-      this.loading = false
-    })
-  }
-}
+      this.year = this.currentTournament.year;
+      const cup = this.currentTournament.mudder_cup;
+      this.showMudderCup = cup.mudder_cup;
+      this.loading = false;
+    });
+  },
+};
 </script>
 <style>
 a {
