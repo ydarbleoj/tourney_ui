fafa1b3635950d2a33a86be1e25d363acc444f11
Joel Brady <brady.joel@gmail.com>
2024-02-23 18:17:18 -0800

    upate

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-
-// ROOT_API: '"https://www.tourney-app.com"'
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  ROOT_API: '"http://localhost:3000"',
-})
--- config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  ROOT_API: '"https://www.tourney-app.com"'
-}
